10f97c16519f81d5a0358d36eff618f4ccad0847
Eviber <youv.didj@gmail.com>
2023-03-02 02:16:31 +0100

    [WIP] setup basic Bevy systems

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@
 
 use bevy::prelude::*;
 
+const TILE_SIZE: f32 = 32.0;
+
 #[derive(Component)]
 struct Player;
 
@@ -13,11 +15,40 @@ enum Direction {
     Right,
 }
 
+#[derive(Component)]
+struct Position {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Component)]
+enum Movement {
+    Up,
+    Down,
+    Left,
+    Right,
+    None,
+}
+
+impl std::ops::AddAssign<&Movement> for Position {
+    fn add_assign(&mut self, other: &Movement) {
+        match other {
+            Movement::Up => self.y -= 1,
+            Movement::Down => self.y += 1,
+            Movement::Left => self.x -= 1,
+            Movement::Right => self.x += 1,
+            Movement::None => (),
+        }
+    }
+}
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup)
         .add_system(move_player)
+        .add_system(move_entity)
+        .add_system(update_transform)
         .add_system(bevy::window::close_on_esc)
         .run();
 }
@@ -34,25 +65,42 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
             ..Default::default()
         },
         Direction::Up,
+        Position { x: 0, y: 0 },
+        Movement::None,
     ));
 }
 
 fn move_player(
     keyboard_input: Res<Input<KeyCode>>,
-    mut player_query: Query<(&Player, &mut Transform)>,
+    mut player_query: Query<(&Player, &mut Movement, &mut Direction)>,
 ) {
-    for (_player, mut transform) in player_query.iter_mut() {
-        if keyboard_input.pressed(KeyCode::Up) {
-            transform.translation.y += 1.0;
-        }
-        if keyboard_input.pressed(KeyCode::Down) {
-            transform.translation.y -= 1.0;
-        }
-        if keyboard_input.pressed(KeyCode::Left) {
-            transform.translation.x -= 1.0;
-        }
-        if keyboard_input.pressed(KeyCode::Right) {
-            transform.translation.x += 1.0;
-        }
+    for (_player, mut movement) in player_query.iter_mut() {
+        *movement = if keyboard_input.pressed(KeyCode::Up) {
+            Movement::Up
+        } else if keyboard_input.pressed(KeyCode::Down) {
+            Movement::Down
+        } else if keyboard_input.pressed(KeyCode::Left) {
+            Movement::Left
+        } else if keyboard_input.pressed(KeyCode::Right) {
+            Movement::Right
+        } else {
+            Movement::None
+        };
+    }
+}
+
+fn move_entity(mut query: Query<(&mut Position, &mut Movement, Option<&mut Direction>)>) {
+    for (mut position, mut movement, mut direction) in query.iter_mut() {
+        // TODO: Check for collisions
+        *position += &movement;
+        movement.x = 0;
+        movement.y = 0;
+    }
+}
+
+fn update_transform(mut query: Query<(&Position, &mut Transform)>) {
+    for (position, mut transform) in query.iter_mut() {
+        transform.translation.x = position.x as f32 * TILE_SIZE;
+        transform.translation.y = position.y as f32 * TILE_SIZE;
     }
 }
